f7d0d6d22aa9bd65fd2e4b5301ba9fd9c6c98d48
haschek <haschek>
2002-12-27 14:11:33 +0000

    no message

---
+++ eyedoc/dialogue.de.inc.php
@@ -0,0 +1,56 @@
+<?php  // Id: /
+
+/*********************************************************/
+/* dialog.de.inc.php                                     */
+/* dialogues [deutsch - de]                              */
+/*********************************************************/
+
+$charset = "iso-8859-1";
+
+// language names : $name_language['code'] = "language name"
+
+$name_language['en'] = "Englisch";
+$name_language['de'] = "Deutsch";
+$name_language['es'] = "Spanisch";
+
+// Menu
+
+$menu['home'] = "Startseite";
+$menu['articles'] = "Artikel";
+$menu['registry'] = "Régister123";
+$menu['search'] = "Stichwörtsuche???";
+$menu['language'] = "Sprache";
+$menu['info'] = "Info über";
+
+// Sub Menu
+
+$submenu['help'] = "Hilfe";
+
+
+// Text flags
+
+$text['datum'] = "Datum";
+$text['author'] = "Autor";
+$text['from'] = "von";
+$text['source'] = "Quelle";
+$text['readmore'] = "weiter lesen";
+$text['searchfor'] = "suchen nach";
+$text['seek'] = "Suche";
+$text['top'] = "hoch";
+$text['help'] = "Hilfe";
+
+// Buttons
+
+$button['search'] = "Suchen!";
+$button['choose_lang'] = "Sprache wählen!";
+
+// Messages
+
+$question = array("0" => "Nach welche Stichworten soll gesucht werden?");
+
+$info = array( "0" => "Keine passenden Einträge gefunden!");
+
+$error = array( "0" => "Datei nicht gefunden.",
+                "1" => "Kann Datei nicht öffnen.");
+
+?>
